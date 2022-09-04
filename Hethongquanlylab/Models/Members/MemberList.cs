06be912e5b65990c9243a0b67b6f4efe9f19b1e4
Administrator <Administrator@NGOXUANHINH2801>
2022-09-04 16:52:41 +0700

    vietsub

--- Hethongquanlylab/Models/Members/MemberList.cs
+++ Hethongquanlylab/Models/Members/MemberList.cs
@@ -11,6 +11,7 @@ namespace Hethongquanlylab.Models
         private int pageSize;
         private int currentPage;
         private List<Member> memberList;
+        private int memberCount;
 
         private String sortOrder;
         private String currentSearchString;
@@ -21,17 +22,29 @@ namespace Hethongquanlylab.Models
         public int PageSize { get => pageSize; set => pageSize = value; }
         public int CurrentPage { get => currentPage; set => currentPage = value; }
         public List<Member> Members { get => memberList; set => memberList = value; }
+        public int MemberCount { get => memberCount; set => memberCount = value; }
 
         public String SortOrder { get => sortOrder; set => sortOrder = value; }
         public String CurrentSearchString { get => currentSearchString; set => currentSearchString = value; }
         public String CurrentSearchField { get => currentSearchField; set => currentSearchField = value; }
         public List<String> SearchFieldList { get => searchFieldList; set => searchFieldList = value; }
 
+
+        public Dictionary<string, string> NameVar { get; set; }
+
         public MemberList()
         {
             this.pageSize = 10;
             this.currentPage = 1;
             this.searchFieldList = new List<String> { "LabID", "Name", "Sex", "Birthday", "Gen", "Unit", "Position"};
+            this.NameVar = new Dictionary<string, string>();
+            this.NameVar.Add("LabID", "LabID");
+            this.NameVar.Add("Name", "Tên thành viên");
+            this.NameVar.Add("Sex", "Giới tính");
+            this.NameVar.Add("Birthday", "Ngày sinh");
+            this.NameVar.Add("Gen", "Thế hệ");
+            this.NameVar.Add("Unit", "Đơn vị");
+            this.NameVar.Add("Position", "Chức vụ");
         }
 
         public void Paging(List<Member> members, int pageSize)
